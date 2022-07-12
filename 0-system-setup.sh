f922e350d1b0b28285b41941c57e0a2aa1aca990
Paul Mandal <paul.mandal@gmail.com>
2022-07-12 16:27:38 -0600

    Add initial version of scripts

---
+++ 0-system-setup.sh
@@ -0,0 +1,51 @@
+#!/bin/bash -e
+source helper-script.conf
+
+if [ ! -f "${targetZip}" ]; then
+  echo "You must be in the same directory as ${targetZip}"
+  exit 1
+fi
+
+# install Docker
+sudo apt-get install \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
+
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/${systemType}/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/${systemType} \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
+
+sudo usermod -aG docker $USER
+
+sudo apt install unzip
+
+# unzip TAKServer
+unzip ${targetZip}
+cd "`echo ${targetZip} | sed 's/\.zip//'`"
+
+# fix TAKServer Docker for Postgres 14.4
+sed -i "s/FROM postgres:.*/FROM postgres:14.4/" docker/Dockerfile.takserver-db
+sed -i "s/postgresql-10-postgis-2.4/postgresql-14-postgis-3/" docker/Dockerfile.takserver-db
+sed -i "s/RUN echo/#RUN echo/" docker/Dockerfile.takserver-db
+sed -i $'s/^\tapt update/RUN apt update/' docker/Dockerfile.takserver-db
+sed -i "s/-t stretch-backports//" docker/Dockerfile.takserver-db
+sed -i "s#postgresql/10/#postgresql/14/#g" tak/db-utils/configureInDocker.sh
+
+# create some useful scripts
+echo '#!/bin/bash' > mk-client-cert.sh && echo 'docker exec -it takserver-${takServerVersion} bash -c "cd /opt/tak/certs && ./makeCert.sh client $1"' >> mk-client-cert.sh && chmod a+x mk-client-cert.sh
+echo '#!/bin/bash' > reload-certs.sh && echo 'docker exec -d takserver-${takServerVersion} bash -c "cd /opt/tak/ && ./configureInDocker.sh"' >> reload-certs.sh && chmod a+x reload-certs.sh
+
+# Update system and reboot
+sudo apt upgrade -y
+sudo apt autoremove -y
+
+echo "Updated system, rebooting!"
+sudo reboot
---
+++ 1-build-and-run-containers.sh
@@ -0,0 +1,30 @@
+#!/bin/bash -e
+source helper-script.conf
+
+echo "Before you run this script: "
+echo " - Edit your tak/CoreConfig.xml and set a db password"
+echo " - Add the SSL input if you want it and comment out the others if you don't want them:"
+echo '         <input _name="stdssl" protocol="tls" port="8089" auth="x509"/>'
+echo " - Edit tak/certs/cert-metadata.sh"
+
+confirmContinue=n
+echo -n "Enter Y to continue: "
+read confirmContinue
+
+if [ "${confirmContinue}" != "y" -a "${confirmContinue}" != "Y" ]
+then
+  exit 1
+fi
+
+cd "`echo ${targetZip} | sed 's/\.zip//'`"
+
+docker build -t takserver-db:${takServerVersion} -f docker/Dockerfile.takserver-db . \
+&& docker network create takserver-net-${takServerVersion} \
+&& mkdir tak-db \
+&& docker run -d -v $(pwd)/tak-db:/var/lib/postgresql/data:z -v $(pwd)/tak:/opt/tak:z -it -p 5432:5432 --network takserver-net-${takServerVersion} --network-alias tak-database --name takserver-db-${takServerVersion} takserver-db:${takServerVersion} \
+&& sudo docker build -t takserver:${takServerVersion} -f docker/Dockerfile.takserver . \
+&& docker run -d -v $(pwd)/tak:/opt/tak:z -it -p 8080:8080 -p 8443:8443 -p 8444:8444 -p 8446:8446 -p 8087:8087 -p 8088:8088 -p 9000:9000 -p 9001:9001 -p 8089:8089 --network takserver-net-${takServerVersion} --name takserver-${takServerVersion} takserver:${takServerVersion} \
+&& docker exec -it takserver-${takServerVersion} bash -c "cd /opt/tak/certs && ./makeRootCa.sh" \
+&& docker exec -it takserver-${takServerVersion} bash -c "cd /opt/tak/certs && ./makeCert.sh server takserver"
+
+echo "su $USER --command='docker start takserver-db-${takServerVersion} takserver-${takServerVersion}'
